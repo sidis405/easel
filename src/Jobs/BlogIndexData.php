5726c1dec371817d1c7cc16417a8c067c2bc7cc0
Todd Austin <austin.todd.j@gmail.com>
2017-08-17 14:37:20 -0500

    Changed addQuery to appends on BlogIndexData

--- src/Jobs/BlogIndexData.php
+++ src/Jobs/BlogIndexData.php
@@ -61,7 +61,7 @@ class BlogIndexData
             ->orderBy('published_at', $reverse_direction ? 'asc' : 'desc')
             ->simplePaginate(config('blog.posts_per_page'));
 
-        $posts->addQuery('tag', $tag->tag);
+        $posts->appends('tag', $tag->tag);
 
         $page_image = $tag->page_image ?: config('blog.page_image');
 
